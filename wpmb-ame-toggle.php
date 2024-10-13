b2c3e1a1eda9ac0a07a7ff0e98b9318fd982ebf1
Josh Robbs <josh@joshrobbs.com>
2024-10-13 15:42:55 -0400

    add classmap

--- wpmb-ame-toggle.php
+++ wpmb-ame-toggle.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPMB AME Toggle
  * Plugin URI: https://wpmasterbuilder.com
  * Description: A simple plugin to create menu toggles with Admin Menu Editor.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: Josh Robbs
  * Author URI: https://wpmasterbuilder.com
  * License: GPL2
